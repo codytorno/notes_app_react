a5beda1c01326c71ebbe162b6f34b81d7745fb23
Cody Torno <ctorno3@gmail.com>
2021-06-21 22:03:22 -0500

    added rest of files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
+import noteServices from "./services/notes";
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
+
+  const updateNotes = () => {
+    console.log("getting notes");
+    noteServices.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+      console.log("notes updated");
+    });
+  };
+
+  useEffect(updateNotes, []);
+  console.log("render", notes.length, "notes");
+
+  // Event Handler -> Adds a note to the notes state variable
+  const addNote = (event) => {
+    console.log("adding note to db");
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString,
+      important: Math.random() < 0.5,
+    };
+
+    noteServices.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+
+  // Event Handler -> updates the value of the input field of the form
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  // Event handler -> Toggles importance button on each item and updates the json server value
+  const handlebuttonToggle = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteServices
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `the note '${note.content}' was already deleted from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
+
+  // holds the notes to be shownbra
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit" onClick={addNote}>
+        save note
+      </button>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "Important" : "All"}
+      </button>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => handlebuttonToggle(note.id)}
+          />
+        ))}
+      </ul>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
